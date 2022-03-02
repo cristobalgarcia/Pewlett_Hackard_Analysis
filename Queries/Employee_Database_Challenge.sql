630708f492768bd89fb4bbb52528ef3002b3a4bb
Cristobal Garcia <cristobalgarcia@Cristobals-MacBook-Pro.local>
2022-03-01 20:22:37 -0800

    adding tables

---
+++ Queries/Employee_Database_Challenge.sql
@@ -0,0 +1,31 @@
+-- SELECT e.emp_no, e.first_name, e.last_name, t.title, t.from_date, t.to_date
+-- INTO retirement_titles
+-- FROM employees as e
+-- INNER JOIN titles AS t
+-- ON (e.emp_no = t.emp_no)
+-- WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+-- ORDER BY emp_no ASC
+
+-- Use Dictinct with Orderby to remove duplicate rows
+-- SELECT DISTINCT ON (rt.emp_no) rt.emp_no,
+-- rt.first_name,
+-- rt.last_name,
+-- rt.title
+
+-- INTO unique_titles
+-- FROM retirement_titles AS rt
+-- ORDER BY emp_no ASC, to_date DESC;
+
+--The Employees Eligible for the Mentorship Program
+SELECT DISTINCT ON(e.emp_no) e.emp_no, e.first_name, e.last_name, e.birth_date, de.from_date, de.to_date, t.title
+INTO mentorship_eligibility
+FROM employees as e
+LEFT OUTER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+LEFT OUTER JOIN titles as t
+ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no;
+
+
+
